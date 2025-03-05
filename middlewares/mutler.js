b3348b82884a524e7d8bcfb6665b2a7ff1ebdcf3
kapil238 <kapilsanghani111@gmail.com>
2025-03-05 15:02:44 +0530

    update mutler

--- middlewares/mutler.js
+++ middlewares/mutler.js
@@ -1,14 +1,4 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-export const singleUpload = upload.single("file"); // Correct export
+const storage = multer.memoryStorage();
+export const singleUpload = multer({storage}).single("file");
