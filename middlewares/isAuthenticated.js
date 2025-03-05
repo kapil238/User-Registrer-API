edc6672bb076a49ec4b4acf786a6664b2e2d9365
kapil238 <kapilsanghani111@gmail.com>
2025-03-05 12:20:53 +0530

    Updated authentication middleware to use headers instead of cookies

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -2,24 +2,34 @@ import jwt from "jsonwebtoken";
 
 const isAuthenticated = async (req, res, next) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
+        const authHeader = req.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith("Bearer ")) {
             return res.status(401).json({
                 message: "User not authenticated",
                 success: false,
-            })
+            });
         }
+
+        const token = authHeader.split(" ")[1];
+
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
-        if(!decode){
+        if (!decode) {
             return res.status(401).json({
-                message:"Invalid token",
-                success:false
-            })
-        };
+                message: "Invalid token",
+                success: false,
+            });
+        }
+
         req.id = decode.userId;
         next();
     } catch (error) {
         console.log(error);
+        return res.status(500).json({
+            message: "Internal Server Error",
+            success: false,
+        });
     }
-}
+};
+
 export default isAuthenticated;
